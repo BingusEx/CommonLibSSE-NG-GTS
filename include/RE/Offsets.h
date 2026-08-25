4dfa43a5456769766303477128afae7dbdc03999
Arial6502 <10480438+Arial6502@users.noreply.github.com>
2026-08-25 23:48:42 +0200

    Fix bad AE offset in console selected ref

--- include/RE/Offsets.h
+++ include/RE/Offsets.h
@@ -152,7 +152,7 @@ namespace RE::Offset
 
 	namespace Console
 	{
-		constexpr auto SelectedRef = RELOCATION_ID(519394, 405935);
+		constexpr auto SelectedRef = RELOCATION_ID(519394, 504099);
 		constexpr auto SetSelectedRef = RELOCATION_ID(50164, 51093);
 	}
 
